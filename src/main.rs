e222fd1009557bf8d62759f1e2d5f26fac4b83ce
Denis <denengine0482@gmail.com>
2023-03-12 19:26:40 +0000

    process backend request

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,8 @@ use tokio::net::TcpListener;
 use tokio_util::sync::CancellationToken;
 // use tokio::signal::unix::{signal, SignalKind};
 use rand::{distributions::Alphanumeric, Rng};
-use tokio::signal;
+// use tokio::signal;
+use std::env;
 use warp::http::StatusCode;
 use warp::Filter;
 
@@ -39,7 +40,7 @@ pub struct AddProxyBody {
     pub username: String,
     pub password: String,
     pub m_port: u16,
-    pub allow_ip: String,
+    pub allow_ip: Vec<String>,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -48,7 +49,18 @@ pub struct CancelProxyBody {
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct ResponseData {
+pub struct BuyResponseData {
+    pub protocol: String,
+    pub host: String,
+    pub port: u16,
+    pub m_username: String,
+    pub m_password: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct StopResponseData {
+    pub protocol: String,
+    pub host: String,
     pub port: u16,
 }
 
@@ -78,15 +90,15 @@ pub async fn run_server(
     m_port: u16,
     username: String,
     password: String,
-    allow_ip: String,
+    allow_ip: Vec<String>,
     proxies: Arc<Mutex<HashMap<u16, ProxyHandle>>>,
-) -> Result<()> {
+) -> Result<(String, String)> {
     let addr: String = format!("{}:{}", host, port).parse()?;
 
     let mut m_username: String = "".to_string();
     let mut m_password: String = "".to_string();
 
-    if allow_ip.is_empty() {
+    if allow_ip.len() == 0 {
         m_username = rand::thread_rng()
             .sample_iter(&Alphanumeric)
             .take(7)
@@ -99,9 +111,13 @@ pub async fn run_server(
             .map(char::from)
             .collect();
 
-        println!("username: {}", m_username);
-        println!("password: {}", m_password);
+        // println!("username: {}", m_username);
+        // println!("password: {}", m_password);
     }
+
+    // This is for using in thread
+    let response_m_username = m_username.clone();
+    let response_m_password = m_password.clone();
 
     let proxy: SocketAddr = addr.parse().unwrap();
 
@@ -128,14 +144,14 @@ pub async fn run_server(
                     };
                     debug!("Got new client connection from {}", src);
 
-                    if !allow_ip.is_empty() {
+                    if allow_ip.len() > 0 {
 
                         if src.is_ipv4() {
                             let ip_str: String = match src.ip() {
                                 IpAddr::V4(ip) => format!("{}.{}.{}.{}", ip.octets()[0], ip.octets()[1], ip.octets()[2], ip.octets()[3]),
                                 IpAddr::V6(_ip) => "".to_string(),
                             };
-                            if ip_str.ne(&allow_ip) {
+                            if!allow_ip.contains(&ip_str) {
                                 error!("Invalid IPv4 address: {}", src.ip());
                                 continue;
                             }
@@ -155,9 +171,9 @@ pub async fn run_server(
                         }
                     });
                 }
-                _ = signal::ctrl_c() => {
-                    break;
-                }
+                // _ = signal::ctrl_c() => {
+                //     break;
+                // }
                 _ = cancel_token_clone.cancelled() => {
                     break;
                 }
@@ -176,7 +192,7 @@ pub async fn run_server(
     }
     proxies.insert(m_port, proxy_handle);
 
-    Ok(())
+    Ok((response_m_username, response_m_password))
 }
 
 pub async fn handle_run_server(
@@ -193,8 +209,14 @@ pub async fn handle_run_server(
     )
     .await
     {
-        Ok(_) => {
-            let data = ResponseData { port: body.m_port };
+        Ok((m_username, m_password)) => {
+            let data = BuyResponseData {
+                protocol: String::from("socks5"),
+                host: env::var("SERVER_PUBLIC_IP").unwrap_or_else(|_| "127.0.0.1".to_string()),
+                port: body.m_port,
+                m_username,
+                m_password,
+            };
             Ok(warp::reply::with_status(
                 warp::reply::json(&data),
                 StatusCode::OK,
@@ -222,7 +244,11 @@ pub async fn handle_stop_server(
     let mut proxies = proxies.lock().unwrap();
     if let Some(handle) = proxies.remove(&port) {
         handle.cancel_token.cancel();
-        let data = ResponseData { port: body.m_port };
+        let data = StopResponseData {
+            protocol: String::from("socks5"),
+            host: env::var("SERVER_PUBLIC_IP").unwrap_or_else(|_| "127.0.0.1".to_string()),
+            port: body.m_port,
+        };
         Ok(warp::reply::with_status(
             warp::reply::json(&data),
             StatusCode::OK,
@@ -238,11 +264,13 @@ pub async fn handle_stop_server(
     }
 }
 
-fn json_add_proxy_body() -> impl Filter<Extract = (AddProxyBody,), Error = warp::Rejection> + Clone {
+fn json_add_proxy_body() -> impl Filter<Extract = (AddProxyBody,), Error = warp::Rejection> + Clone
+{
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
 }
 
-fn json_cancel_proxy_body() -> impl Filter<Extract = (CancelProxyBody,), Error = warp::Rejection> + Clone {
+fn json_cancel_proxy_body(
+) -> impl Filter<Extract = (CancelProxyBody,), Error = warp::Rejection> + Clone {
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
 }
 
@@ -262,9 +290,20 @@ pub async fn main() -> Result<()> {
 
     let proxies: Arc<Mutex<HashMap<u16, ProxyHandle>>> = Arc::new(Mutex::new(HashMap::new()));
     let proxies_clone = proxies.clone();
+    // let test_proxies = proxies.clone();
+    // let test_body: AddProxyBody = AddProxyBody {
+    //     host: String::from("ip"),
+    //     port: 56250,
+    //     m_port: port,
+    //     username: String::from(""),
+    //     password: String::from(""),
+    //     allow_ip: vec![String::from("209.145.59.9")],
+    // };
+
+    // handle_run_server((test_body, test_proxies)).await?;
 
     let api_run_server = warp::post()
-        .and(warp::path!("api" / "proxy" / "buy"))
+        .and(warp::path!("api" / "buy"))
         .and(warp::path::end())
         .and(json_add_proxy_body())
         .map(move |body: AddProxyBody| {
@@ -274,7 +313,7 @@ pub async fn main() -> Result<()> {
         .and_then(handle_run_server);
 
     let api_stop_server = warp::post()
-        .and(warp::path!("api" / "proxy" / "stop"))
+        .and(warp::path!("api" / "stop"))
         .and(warp::path::end())
         .and(json_cancel_proxy_body())
         .map(move |body: CancelProxyBody| {
